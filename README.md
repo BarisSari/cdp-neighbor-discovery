7e376d818655f474bee29893e2d46d83ff8f72f2
Baris Sari <bayrambariss@gmail.com>
2018-07-19 10:40:00 +0300

    Files are updated

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# cdp-neighbor-discovery
+# CDP Neighbor Discovery
+
+The developed project is to implement the network automation, which is now trending, in the existing system and make the tasks easier. In the first part of this project, Python script is traversing all Cisco Network Devices with the help of Cisco Discovery Protocol (CDP). CDP shows all the neighbor devices that connected. After neighbors are found and their IPs are known, hostname-IP matching is applied and written to the file for all IPs in the second part of the project. Besides that, in the second part, fully qualified domain names (FQDNs) are also written to a different file.
+
+As it has mentioned above, CDP is a powerful protocol because it helps the user to find information about neighbor devices of a device. Therefore, it is possible to reach details of a device from its neighbors. 
+
+### First Part
+
+![open-session](images/open-session.jpg)
+
+This function is implemented in order to SSH Connection to a device. Paramiko library, i.e. a Python library for SSH, is used for SSH Connection. Function tries to connect with given hostname, port number, username and password. If there is an error, exceptions occurs and function returns false. Possible exceptions, i.e. Authentication exception, No valid connection, Timeout error, were tested and added to the exceptions. When an exception occurs, it print outs why connection failed.
+
+![extract-neighbors](images/extract-cdp-neigh.jpg)
+
+This function firstly calls open_session function in order to connect the given IP. If connection is successful, it tries to execute “show cdp neighbors” command with exec_command() function of Paramiko library. Since it returns output as bytes, it is read and decoded to string. After the conversion from bytes to string, Regular Expression, “re” library of Python is used for capturing interface names from the output. First seventeen characters are skipped and then the lines which contain the words that start with Gi, Te, Vl, Loop or F are found. These are first two characters of the possible interface names. Thus, the next fifteen characters are taken and added to the list of interface_name. re.finditer finds matching lines in the output with the regular expression rule. Function returns this list. Possible exception for this function is SSH exception. Therefore, try/except is used for implementation.
+
+![neighbor-detail](images/neigh-detail.jpg)
+
+Neighbor_detail() takes one extra parameters rather than taking only an IP to connect, it takes “commands” parameter because it executes multiple line of commands when SSH connection is successful to an IP, i.e. Cisco Network Device. Find_ips(), the main function of this part of the project, creates these commands. It firstly connects to the device, and then tries to open an interactive shell. Paramiko’s invoke_shell() function does this job. When shell is opened, stdin, and output are directly connects to the device. So, commands are written to the input with this format (The variable“formatted_commands” has a string with this format):
+
+![input-1](images/input-format.jpg)
+
+As it explained before, byte to string conversion is necessary for applying regular expression to the output. This time, regular expression matches the lines with " IP address: xxx" and takes the IP addresses from that matches. After the IP address is found, it is added to the list if it is not in the list of discovered IPs.
+
+![find-ips](images/find-ips.jpg)
+
+Find_ips() is the main function of this part of the project. It runs extract_cdp_neigbors() function and takes all the interface names. If interface names list is empty, function returns -1. If function continues to run, it creates “show cdp neighbors XXX detail | include IP” commands for each interface name in the list. Finally it adds “exit” command to the commands and these commands starts to execute in neighbor_detail() function.
+
+### Second Part
+
+The purpose of the second part of the project is to find all IP addresses, interfaces and match them with the hostnames and domain names. The command "show ip interface brief | exlclude unassigned" should be run for this matching. 
+
+![get-hostname-domainname](images/get-hostname-domainname.jpg)
+
+This function connects to a device and opens an interactive shell like neighbor_detail() function. When shell is opened, it runs “show run | i hostname” and “show run | i domain-name” commands. Commands are converted to proper format for interactive shell. 
+
+A trick is used to get rid of escape characters of Windows. When output is read and decoded to string, each line ends with “\r\n”. When string.splitlines() applied, it turns into a list without escape characters. Then, ‘\n’.join() is used to turn it back to a string without ‘\r’. Finally, from this output, hostnames and domain names are captured with proper regex rules. Function returns these findings.
+
+![match-names](images/matched.jpg)
+
+This function takes two extra parameters: hostname and domain name from get_hostname_and_domain_name() function. It executes only one command which is “show ip interface brief | exclude unassigned”. Output is read, decoded and ‘\r’ character is deleted from each line like the function above. After that, regular expression is applied but there are two different group of matches this time, interface and IP address. When matching is done, it is necessary to abbreviate interface. For instance, TenGigabitEthernet1/1 and Vlan50 are interface names. They must be shortened with this format: first two letters and the numbers at the end, for this example: Te1/1 and Vl50. Since ‘/’ character is not used for this network, this character is replaced with ‘\_’. Finally, “hostname-shortened_interface.domain-name   IP” is created, added to a list, and returned by this function.
+
+![write-file](images/write-file.jpg)
+
+write_file() is the main function of the second part of the project. It first calls get_hostname_and_domain_name(). When hostname and domain name are existed and returned, it calls match_name_with_ip_address() if hostname is not in the hostnames list. 
+
+![main](images/main.jpg)
+
+Main function starts with reading ‘ip.txt’ file that contains one IP address inside. This address is added to IP list and two files are opened to write IP list, FQDN list and matched list. After that, a Thread Pool is created for concurrency. Concurrency is a technology that allows multiple processes to run at the same time in the coded program. For instance, the code usually is executed step by step. In each step, previous step must be finished for next one to start, but when a thread is used, these steps work in parallel. This program starts to run concurrently after the size of IP list exceeds fifteen. Until then, it calls find_ips() function and add new IPs to the list. 
+In the second while loop, up to fifteen threads work together at the same time and finally, all the connected Cisco Network Devices is traversed. After that, up to fifteen threads work together again for matching hostnames with IP addresses. When all the processes are done, IP list, FQDN list, and matched list is written to different files. Execution time is written at the end of matched list. 
+
---
+++ multithreading_cdp.py
@@ -0,0 +1,315 @@
+# Developed by Bayram Barış Sarı
+# E-mail = bayrambariss@gmail.com
+# Tel = +90 539 593 7501
+#
+# This file traverses Cisco network devices with the help of CDP.
+# It reads an IP from ip.txt file and starts traversing from there.
+# After it connects to that IP, it runs 'show cdp neighbors' and
+# extracts the interface names. Then it creates and runs a new
+# command for each interface name. For instance:
+# 'show cdp neighbors Gig 0/9 detail | include IP'
+# If the IP address is not on the list of ip_addresses,
+# it adds the IP to the list. This process continues until all the
+# IP addresses on the list are traversed.
+#
+# After IP list created, it is written to the file and then ???????
+# For each time an IP is visited, the functions in matching.py work.
+# Hostname and domain name of the IP address are found. Then all the
+# interface names are found and are abbreviated. Finally, a list
+# that contain this kind of lines is created by matchin.py function:
+#
+# hostname + '-' + abbreviated interface name + '.' + domain_name + a separator + IP address
+
+
+from multiprocessing.pool import ThreadPool
+import paramiko
+import configparser
+import re
+import time
+
+# take data for ssh connection from credentials.ini file
+cfg = configparser.ConfigParser()
+cfg.read('credentials.ini')
+
+username = cfg['DEFAULT']['username']
+password = cfg['DEFAULT']['password']
+default_domain_name = cfg['DEFAULT']['domain-name']
+port = cfg['DEFAULT']['port']
+
+ip_list = []
+hostname_list = []
+fqdn_list = []
+matched_list = []
+
+
+def open_session(hostname):
+        try:
+        print("Connected to:{}".format(hostname))
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=hostname, port=port, username=username, password=password)
+        return ssh, True
+    except paramiko.ssh_exception.AuthenticationException:
+        print("Authentication to IP:{ip} failed! Please check your hostname, username and password.".format(ip=hostname))
+        return None, False
+    except paramiko.ssh_exception.NoValidConnectionsError:
+        print("Unable to connect to IP:{ip}".format(ip=hostname))
+        return None, False
+    except (ConnectionError, TimeoutError):
+        print("Timeout error occured for IP:{ip}!".format(ip=hostname))
+        return None, False
+
+
+def extract_cdp_neighbors(ip):
+    interface_names = []
+    command = 'show cdp neighbors'
+    # print("This command is going to be executed: '{0}'".format(command))
+    # skip first 17 characters, then take the next 17 characters which start with Gi,Te,Vl,Loop or F
+    regex = r"^.{17}(\b(Ten|Gig|Loo|Vla).{15})"
+    # try to connect to server, if there is no connection, return none
+    ssh, connection = open_session(ip)
+    if not connection:
+        return None
+    try:
+        _, output, _ = ssh.exec_command(command)
+        output = output.read()
+        output = output.decode("utf-8")
+        # find matching lines in output with regex rule
+        matches = re.finditer(regex, output, re.MULTILINE)
+        for match in matches:
+            # delete the whitespace characters at the end
+            temp_interface_name = match.group(1)
+            temp_interface_name = temp_interface_name.strip()
+            # add the name to the interface_name list
+            interface_names.append(temp_interface_name)
+        return interface_names
+    except paramiko.ssh_exception.SSHException:
+        print("Extract CDP Neighbor Function Error:There is an error connecting or establishing SSH session")
+        return None
+    finally:
+        ssh.close()
+
+
+def neighbor_detail(ip, commands):
+    formatted_commands = []
+    global ip_list
+    # print("Now, this command is going to be executed: '{0}'".format(commands))
+    # regular expression rule for finding lines with " IP address: xxx"
+    regex = r"(?=[\n\r].*IP address:[\s]*([^\n\r]*))"
+    # try to connect to server, if there is no connection, return none
+    ssh, connection = open_session(ip)
+    if not connection:
+        return None
+    try:
+        channel = ssh.invoke_shell()
+        stdin = channel.makefile('wb')
+        output = channel.makefile('rb')
+
+        formatted_commands.append("'''")
+        for c in commands:
+            formatted_commands.append(c)
+        formatted_commands.append("'''")
+        formatted_commands = '\n'.join(formatted_commands)
+        stdin.write(str.encode(formatted_commands))
+        output = output.read()
+        output = output.decode('utf-8')
+        stdin.close()
+        matches = re.finditer(regex, output, re.MULTILINE)
+        i = 1
+        for match in matches:
+            if match.group(i):
+                found_ip = match.group(i)
+                # add the found IP if it's not in the list
+                if found_ip not in ip_list:
+                    ip_list.append(found_ip)
+    except paramiko.ssh_exception.SSHException:
+        print("Neighbor Detail Function Error:There is an error connecting or establishing SSH session")
+    finally:
+        ssh.close()
+
+
+def find_ips(ip):
+    commands = []
+    interface_names = extract_cdp_neighbors(ip)
+    # print(interface_names)
+    # if there is no neighbor, continue with the next IP in the ip_addresses list
+    if not interface_names:
+        return -1
+    # for all interface names, find their IP and add to the ip_addresses list
+    for name in interface_names:
+        commands.append('show cdp neighbors ' + name + ' detail | include IP')
+    commands.append('exit')
+    neighbor_detail(ip, commands)
+
+
+def get_hostname_and_domain_name(ip):
+    hostname = None
+    domain_name = default_domain_name
+    regex_hostname = r"^\bhostname[\s\r]+(.*)$"
+    regex_domain_name = r"^ip[\s\r]domain-name[\s\r]+(.*)$"
+    # try to connect to server, if there is no connection, return none
+    ssh, connection = open_session(ip)
+    if not connection:
+        return '-1', default_domain_name
+    try:
+        channel = ssh.invoke_shell()
+        stdin = channel.makefile('wb')
+        output = channel.makefile('rb')
+        stdin.write('''
+        show run | i hostname
+        show run | i domain-name
+        exit
+        ''')
+
+        output = output.read()
+        output = output.decode("utf-8").splitlines()
+        output = '\n'.join(output)
+        hostname_matches = re.finditer(regex_hostname, output, re.MULTILINE)
+        for h in hostname_matches:
+            hostname = h.group(1)
+
+        domain_name_matches = re.finditer(regex_domain_name, output, re.MULTILINE)
+        for d in domain_name_matches:
+            domain_name = d.group(1)
+
+        stdin.close()
+        return hostname, domain_name
+    except paramiko.ssh_exception.SSHException:
+        print("There is an error connecting or establishing SSH session")
+    finally:
+        ssh.close()
+
+
+def match_name_with_ip_address(ip, hostname, domain_name):
+    temp_data = []
+    command = 'show ip interface brief | exclude unassigned'
+    # print("\nNow, this command is going to be executed: '", command,"'")
+    # take the first 25 characters which start with G,T,V,L or F
+    regex = r"(^[GTVLF].{22})+(.{16})"
+    # try to connect to server, if there is no connection, return none
+    ssh, connection = open_session(ip)
+    if not connection:
+        return None
+    try:
+        _, output, _ = ssh.exec_command(command)
+        output = output.read()
+        output = output.decode("utf-8").splitlines()
+        output = '\n'.join(output)
+
+        # find matching lines in output with regex rule
+        matches = re.finditer(regex, output, re.MULTILINE)
+        for match in matches:
+            # print ("Match: {match}".format(match = match.group()))
+
+            # temp interface means temp interface name
+            temp_interface = match.group(1)
+            temp_interface = temp_interface.strip()
+
+            # temp ip means the ip number of the temp interface
+            temp_ip = match.group(2)
+            temp_ip = temp_ip.strip()
+
+            # shortened means the first the characters of the temp interface name
+            shortened = temp_interface[0:2]
+
+            # temp no means the numbers at last of the interface name, i.e xxxx1/29 to 1/29, xxxxx3 to 3
+            temp_no = []
+            # this loop parses the temp interface name from the end and takes the characters for temp no
+            for j in range(1, len(temp_interface)):
+                if temp_interface[-j] == '/' or temp_interface[-j].isdigit():
+                    temp_no.append(temp_interface[-j])
+
+            # temp name means the shortened+numbers of the interface name, i.e TenGigabitEthernet1/1 to Te1_1
+            temp_name = shortened + ''.join(temp_no[::-1])
+            temp_name = temp_name.replace('/', '_')
+            # name means this type of data: istnswbb0001-te1_1.euea.corp.bshg.com
+            name = hostname + '-' + temp_name.lower() + '.' + domain_name
+            data = name + '\t' + temp_ip
+            # print(data)
+            temp_data.append(data)
+
+        return temp_data
+    except paramiko.ssh_exception.SSHException:
+        print("There is an error connecting or establishing SSH session")
+    finally:
+        ssh.close()
+
+
+def write_file(ip):
+    global fqdn_list
+    hostname, domain_name = get_hostname_and_domain_name(ip)
+    if hostname == '-1':
+        return -1
+    elif not hostname:
+        print("Hostname couldn't be found!")
+        return -2
+    elif hostname not in hostname_list:
+        hostname_list.append(hostname)
+        print("Hostname:", hostname)
+        fqdn = '%s'%hostname
+        fqdn += '.%s'%domain_name
+        fqdn_list.append(fqdn)
+        
+        lines_to_write = match_name_with_ip_address(ip, hostname, domain_name)
+        for line in lines_to_write:
+            matched_list.append(lines_to_write)
+    else:
+        print("Hostname:{0} is in the list of hostnames".format(hostname))
+        return -3
+
+
+def main():
+    global ip_list
+    # ip for first ssh connection
+    with open('ip.txt') as f:
+        ip = f.readline()
+    ip_list.append(ip)
+
+    ip_filename = "found_ips_multithreading_" + ip + ".txt"
+    ip_file = open(ip_filename, "w")
+
+    fqdn_filename = "fqdn_multithreading_" + ip + ".txt"
+    fqdn_file = open(fqdn_filename, "w")
+
+    dns_filename = "dns_multithreading_" + ip + ".txt"
+    dns_file = open(dns_filename, "w")
+
+    pool = ThreadPool(15)
+    i = 0
+
+    start = time.time()
+    while i < len(ip_list) < 15:
+        find_ips(ip_list[i])
+        i = i + 1
+
+    while i < len(ip_list):
+        limit = i+min(15, (len(ip_list)-i))
+        hostnames = ip_list[i:limit]
+        pool.map(find_ips, hostnames)
+        i = limit
+
+    pool.map(write_file, ip_list)
+    pool.close()
+    pool.join()
+
+    end = time.time()
+    elapsed = (end-start)/60
+    string = "\nTotal execution time: {:.7} minutes.".format(elapsed)
+    print(string)
+    
+    for ip in ip_list:
+        ip_file.write(ip + '\n')
+    ip_file.close()
+
+    for fqdn in fqdn_list:
+        fqdn_file.write(fqdn + '\n')
+    fqdn_file.close()
+
+    for match in matched_list:
+        dns_file.write(match.strip() + '\n')
+    dns_file.write(string)
+    dns_file.close()
+
+
+if __name__ == "__main__":
+    main()
